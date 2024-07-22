f2e66031daa4bfe77fbf992dba3a80be797f0b51
David Rivero <david.davidsociety.rivero@gmail.com>
2024-07-22 19:27:27 +0100

    Add colors, fonts and assets for landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,23 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'q-text': '#040316',
+        'q-background': '#fbfbfe',
+        'q-background-v2': '#dddbff',
+        'q-primary': '#ca81be',
+        'q-secondary': '#dddbff',
+        'q-accent': '#443dff',
+        'q-footer': '#40315C',
+        'q-second': '#E85341',
+      },
+    },
+    fontFamily: {
+      'raleway': ['Raleway'],
+      'raleway-bold': ['"Raleway Bold"'],
+      'raleway-bold': ['"Raleway Black"'],
+    }
   },
   plugins: [],
 };
