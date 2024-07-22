91a8ce3fb96f4d0449f4917a2b189e2c208e0e52
David Rivero <david.davidsociety.rivero@gmail.com>
2024-07-22 19:49:46 +0100

    Fix props definition in SectionTitle.

--- app/common/components/product/SectionTitle.tsx
+++ app/common/components/product/SectionTitle.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { PropsWithChildren } from 'react';
 
-export const SectionTitle: React.FC = ({ children }: Readonly<{ children: React.ReactNode; }>) => {
+export const SectionTitle: React.FC<PropsWithChildren<any>> = ({ children }) => {
   return (
     <h2 className="text-center text-4xl mb-12 font-['Raleway_Bold']">{children}</h2>
   );
